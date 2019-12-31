7bddedf42c8bcb9fb99e533e85bd096b778eec36
Fer <jampasnando@gmail.com>
2019-12-31 15:58:17 -0400

    con foto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { File } from '@ionic-native/file/ngx';
 import { Camera } from '@ionic-native/camera/ngx';
+import { IonicStorageModule } from '@ionic/storage';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
     BarcodeScanner,
     File,
     Camera,
+    WebView,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/inventario/inventario.page.ts
+++ src/app/inventario/inventario.page.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { GLOBAL } from '../service/global';
 import { Router } from '@angular/router';
 import { Camera, CameraOptions, DestinationType, EncodingType, MediaType } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { Storage } from '@ionic/storage';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+const STORAGE_KEY="misimagenes";
 @Component({
   selector: 'app-inventario',
   templateUrl: './inventario.page.html',
@@ -9,11 +13,15 @@ import { Camera, CameraOptions, DestinationType, EncodingType, MediaType } from 
 })
 export class InventarioPage implements OnInit {
 
-  constructor(private router:Router,camera:Camera) { }
+  constructor(private router:Router,private camera:Camera, private file:File,public storage:Storage,private webview:WebView) { }
   aux:any=GLOBAL;
+  archivo:any;
+  arr:[];
+  linkimg:string;
+  @Input() useURI = true;
   camopt:CameraOptions={
     quality:20,
-    destinationType:DestinationType.DATA_URL,
+    destinationType:this.useURI ? this.camera.DestinationType.FILE_URI : this.camera.DestinationType.DATA_URL,
     encodingType:EncodingType.JPEG,
     mediaType:MediaType.PICTURE
 
@@ -35,5 +43,46 @@ export class InventarioPage implements OnInit {
   }
   foto(item){
     console.log(item);
+    this.camera.getPicture(this.camopt).then((imagen)=>{
+      var nombreactual=imagen.substr(imagen.lastIndexOf('/')+1);
+      var dirfoto=imagen.substr(0,imagen.lastIndexOf('/')+1);
+      var d=new Date();
+      var n=d.getTime();
+      var nombrearch=n+".jpg";
+      console.log("imagen: ",imagen);
+      console.log("dirfoto: ",dirfoto," nombreactual: ",nombreactual," dataDirectory: ",this.file.dataDirectory," nombrearch: ",nombrearch);
+      this.file.copyFile(dirfoto,nombreactual,this.file.dataDirectory,nombrearch).then(_ =>{
+        alert("foto copiada");
+        this.storage.get(STORAGE_KEY).then(images=>{
+          let arr=JSON.parse(images);
+          console.log("de la memoria: ",arr);
+          if(!arr){
+            let nuevaimg=[nombrearch];
+            this.storage.set(STORAGE_KEY,JSON.stringify(nuevaimg));
+          }
+          else{
+            arr.push(nombrearch);
+            this.storage.set(STORAGE_KEY,JSON.stringify(arr));
+          }
+        });
+        let dirlocalfoto=this.file.dataDirectory+nombrearch;
+        let dirfotonormalizado=this.webview.convertFileSrc(dirlocalfoto);
+        alert(dirfotonormalizado);
+        this.linkimg=(window as any).Ionic.WebView.convertFileSrc(imagen);
+        console.log("dirlocalfoto: ",dirlocalfoto," dirfotonormalizado: ",dirfotonormalizado);
+      },
+      error=>{
+        alert("error copiando foto");
+      }
+      );
+        // this.archivo = (window as any).Ionic.WebView.convertFileSrc(imagen);
+      
+    },
+    (error)=>{
+      alert("error camara");
+    }
+    );
+    
+
   }
 }
